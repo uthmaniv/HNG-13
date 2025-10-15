8cf4b8eca46f93ecb8b9ed4943824ae073883ebc
uthmaniv <uthmanyahayababa@gmail.com>
2025-10-15 22:52:18 +0100

    Initial commit

---
+++ stage_0/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, Response
+import requests
+from datetime import datetime
+from collections import OrderedDict
+import json
+
+app = Flask(__name__)
+
+@app.route('/me', methods=['GET'])
+def get_profile():
+    # Fetch random cat fact from external API
+    try:
+        response = requests.get('https://catfact.ninja/fact', timeout=5)
+        response.raise_for_status()  # raises HTTPError if bad response
+        cat_fact = response.json().get('fact', 'No cat fact available.')
+    except requests.RequestException:
+        cat_fact = "Could not fetch cat fact at this time."
+
+    # Prepare response data
+    data = OrderedDict([
+        ("status", "success"),
+        ("user", {
+            "email": "uthmanyahaya@gmail.com",
+            "name": "Usman Yahaya",
+            "stack": "Python/Flask"
+        }),
+        ("timestamp", datetime.utcnow().isoformat() + "Z"),
+        ("fact", cat_fact)
+    ])
+
+    return Response(json.dumps(data), mimetype='application/json'), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
